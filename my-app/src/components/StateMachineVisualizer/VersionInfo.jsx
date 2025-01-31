df600873943970f1afd2ce3a07339085fc49d6ec
uhaseeb85 <uhaseeb85@gmail.com>
2025-01-30 23:30:58 -0800

    updated version info

--- my-app/src/components/StateMachineVisualizer/VersionInfo.jsx
+++ my-app/src/components/StateMachineVisualizer/VersionInfo.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
 export default function VersionInfo() {
-  const commitId = "beta"; // Update this with your latest commit ID
-  const version = "0.0.3";    // Update this with your version number
+  const version = "v 0.5 Beta";    // Update this with your version number
 
   return (
     <div className="fixed bottom-4 left-4 text-xs text-gray-400 dark:text-gray-600">
-      v{version} ({commitId})
+      {version}
     </div>
   );
 } 
